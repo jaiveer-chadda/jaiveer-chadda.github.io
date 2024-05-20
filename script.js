47935fd8c3863086fd93f462730963cbee1e0dc4
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2024-05-20 19:01:01 +0400

    Added animations, link, and framework for new page

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const topLeftBox = document.getElementById('top_left_box');
+    const introText = document.getElementById('intro');
+
+    topLeftBox.addEventListener('mouseenter', () => {
+        introText.style.opacity = '0';
+        setTimeout(() => {
+            introText.textContent = 'About me...';
+            introText.style.fontSize = '6vw';
+            introText.style.opacity = '1';
+        }, 350);
+    });
+
+    topLeftBox.addEventListener('mouseleave', () => {
+        introText.style.opacity = '0';
+        setTimeout(() => {
+            introText.textContent = "Hi, I'm JV!";
+            introText.style.fontSize = '6.5vw';
+            introText.style.opacity = '1';
+        }, 350);
+    });
+});
